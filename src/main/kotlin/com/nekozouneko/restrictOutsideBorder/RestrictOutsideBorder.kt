42b11196303861a35f82c68a53a9779b470523fd
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-12-26 13:18:49 +0900

    Initial Project

---
+++ src/main/kotlin/com/nekozouneko/restrictOutsideBorder/RestrictOutsideBorder.kt
@@ -0,0 +1,14 @@
+package com.nekozouneko.restrictOutsideBorder
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RestrictOutsideBorder : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
